c0015f8b255a76ebbc498370343cc5df7e3635f2
balintbiro <biro.balint.szie@gmail.com>
2021-11-08 16:11:45 +0100

    z score correction
    Z score =(element-population mean)/(population SD/samplesize)
    
    So two informations about the population are required! Which is usually not possible.

--- codes/z_score_calculation.R
+++ codes/z_score_calculation.R
@@ -2,13 +2,17 @@
 #-the number of standard deviations from the population mean for a given data point
 
 #define a simple variable
-my_variable<-c(2,2,3,2,5,1,6)
+my_population<-c(2,1,3,1,2,3,1,3,4,5,1,1,3,4,5,2,3,4,5,2,3,2,4,1,3,4,4,2,4,5,45,3,2,1,5)
+
+#setting the seed
+set.seed(1)
+my_sample<-sample(population,size=10,replace=FALSE)
 
 #define a function for calculating z scores for every data point in a given variable
-calc_z_score <- function(input_element,input_variable){
-  z_score<-(input_element-mean(input_variable))/sd(input_variable)
+calc_z_score <- function(input_element,sample,population){
+  z_score<-(input_element-mean(population))/(sd(population)/length(sample))
   return(z_score)
 }
 
 #apply the previously defined function to all element in a variable
-z_scores<-sapply(my_variable,calc_z_score, input_variable=my_variable)
+z_scores<-sapply(my_sample,calc_z_score, sample=my_sample,population=my_population)
