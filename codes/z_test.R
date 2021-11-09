c4e92110071f21bd1d58ef347deea25521629d99
balintbiro <biro.balint.szie@gmail.com>
2021-11-09 09:51:15 +0100

    one sample z test

--- codes/z_test.R
+++ codes/z_test.R
@@ -4,9 +4,13 @@ library(TeachingDemos)
 
 #compare our value to a hypothetical one (one sample version) or to another one (two samples version)
 #condition(s) of usage:
-#-normally distributed variable with a known standard deviation
+#-normally distributed variable with a known standard deviation-->population SD!
 
 #one sample version
-#in this case we compare our value to a hypothetical one
-my_variable<-c(1,3,1,3,4,1,1,2,4,1,2,3,4,1,2,4,2,4,2,1,3,4,4,1,3,4)
-z.test()
+#in this case we compare population mean (u) to a hypothetical value (u0)
+#H0 is that u=u0
+#H1 two sided u!=u0
+#H1 ones sided u>u0; u<u0
+my_population<-c(1,3,1,3,4,1,1,2,4,1,2,3,4,1,2,4,2,4,2,1,3,4,4,1,3,4)
+z.test(my_population,mu=16,stdev=5)#mu and stdev are hypothetical values
+
