30cda5d71236b6b58a4010378b2bc22ae0a4abe5
balintbiro <biro.balint.szie@gmail.com>
2021-11-16 16:37:07 +0100

    saving plot

--- codes/normality_testing.R
+++ codes/normality_testing.R
@@ -1,16 +1,26 @@
 #qqplot
 #quantile is a "line" which divides the datapoints into equally sized groups
 #for example percentiles are quantiles which divide the data into 100 equally sized groups
-#built in qq plot
+#built in qq plot for comparing quantiles of two variables
+setwd('../statistics/results/')
 set.seed(1)
 population<-seq(1,1000,1)
 var_a<-sample(population,size=100,replace=TRUE)
 var_b<-sample(population,size=100,replace=TRUE)
+png(file='qqplot_built_in.png',
+    width=75,
+    height=25)
 qqplot(var_a,var_b)
 abline(0,1)
+dev.off()
 
-#diy qqplot<-sort the variables and then plot them
+#diy qqplot for comparing quantiles of two variables
+#sort the variables and then plot them
 var1<-sort(var_a,decreasing=FALSE)
 var2<-sort(var_b,decreasing=FALSE)
 plot(var1,var2)
 abline(0,1)
+
+#it is also possible to compare the quantiles of a known distribution with our samples
+#to do that first we have to generate a vector which is distributed in a way we like it to be
+var_a
