e1db5fdaae2baf296bba0b0546fb85231c1ca1e8
balintbiro <biro.balint.szie@gmail.com>
2021-11-16 16:44:08 +0100

    saving built in and diy qqplots

--- codes/normality_testing.R
+++ codes/normality_testing.R
@@ -7,9 +7,8 @@ set.seed(1)
 population<-seq(1,1000,1)
 var_a<-sample(population,size=100,replace=TRUE)
 var_b<-sample(population,size=100,replace=TRUE)
-png(file='qqplot_built_in.png',
-    width=75,
-    height=25)
+png(file='qqplot_built_in.png')
+par(mar=c(1, 1, 1, 1))
 qqplot(var_a,var_b)
 abline(0,1)
 dev.off()
@@ -18,8 +17,10 @@ dev.off()
 #sort the variables and then plot them
 var1<-sort(var_a,decreasing=FALSE)
 var2<-sort(var_b,decreasing=FALSE)
+png(file='qqplot_diy.png')
 plot(var1,var2)
 abline(0,1)
+dev.off()
 
 #it is also possible to compare the quantiles of a known distribution with our samples
 #to do that first we have to generate a vector which is distributed in a way we like it to be
