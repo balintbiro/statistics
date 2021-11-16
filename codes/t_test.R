5d3bd0dff1654583490c18e11a1bcc8a00feeddc
balintbiro <biro.balint.szie@gmail.com>
2021-11-16 15:10:26 +0100

    commenting

--- codes/t_test.R
+++ codes/t_test.R
@@ -3,12 +3,12 @@
 #-normally distributed variable with unknown standard deviation
 #t score<-(sample_mean-hypothetical_value)/(sample_standard_deviation/sqrt(samplesize))
 
-my_variable<-c(2,7,10,0,6,0,4,5,6,2)
+my_variable<-c(3,7,11,0,7,0,4,5,6,2)
 
+#in this case our H0: u=3; H1: u!=3
+#this is a two sided alternative hypothesis since we do not let equality
+#doesnt mattwer whther it is smaller or bigger
 n<-length(my_variable)
-hypothetical_value<-4
-t_stat<-mean(my_variable)-hypothetical_value/sd(my_variable)/sqrt(n)
+hypothetical_value<-3
+t_stat<-(mean(my_variable)-hypothetical_value)/(sd(my_variable)/sqrt(n))
 
-install.packages('distributions3')
-library(distributions3)
-T_9<-StudentsT(df=9)
