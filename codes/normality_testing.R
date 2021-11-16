4d46472d392906d526971f2bceffda928630d3d1
balintbiro <biro.balint.szie@gmail.com>
2021-11-16 17:00:56 +0100

    qqplot for known distribution (not finished)

--- codes/normality_testing.R
+++ codes/normality_testing.R
@@ -24,4 +24,10 @@ dev.off()
 
 #it is also possible to compare the quantiles of a known distribution with our samples
 #to do that first we have to generate a vector which is distributed in a way we like it to be
-var_a
+var_a<-dnorm(seq(100,150,1),
+             mean=2.5,
+             sd=0.5)
+var_b<-seq(from=min(var_a),
+           to=max(var_a),
+           length.out=100)
+qqplot(var_a,var_b)
