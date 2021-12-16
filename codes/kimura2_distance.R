6e57e5140a8ca57ff6fee0175bda42fc71a5a650
balintbiro <biro.balint.szie@gmail.com>
2021-12-16 13:10:13 +0100

    use vectors instead of strings

--- codes/kimura2_distance.R
+++ codes/kimura2_distance.R
@@ -1,7 +1,11 @@
 
 #given the two sequences. Let's calculate the Kimura2 distance between them!
-seq1<-'GCCGTCAGAAATTTAGCACTGATCACAGCCTCGTCTCTGA'
-seq2<-'GCCCTCAGGGAATTAGCACTAATCATAACTCCGTCTGTGT'
+seq1<-unlist(strsplit('GCCGTCAGAAATTTAGCACTGATCACAGCCTCGTCTCTGA',
+                      split=""))
+seq2<-unlist(strsplit('GCCCTCAGGGAATTAGCACTAATCATAACTCCGTCTGTGT',
+                      split=""))
 
-#define the purin and pirimidin bases
-purin<-
+#define the purin and pyrimidin bases
+purins<-c('A','G')
+pyrimidines<-c('C','T')
+
