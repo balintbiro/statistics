6ddd061b95e07e63edf2381587588846d081860e
balintbiro <biro.balint.szie@gmail.com>
2021-12-16 13:39:19 +0100

    conditions for transitions

--- codes/kimura2_distance.R
+++ codes/kimura2_distance.R
@@ -9,3 +9,19 @@ seq2<-unlist(strsplit('GCCCTCAGGGAATTAGCACTAATCATAACTCCGTCTGTGT',
 purins<-c('A','G')
 pyrimidines<-c('C','T')
 
+#define function for calculating Kimura 2 genetic distance
+kimura_dist<-function(index){
+  nucleotide_a<-seq1[index]
+  nucleotide_b<-seq2[index]
+  if (nucleotide_a!=nucleotide_b){
+    if (((nucleotide_a %in% purins) & (nucleotide_b %in% purins))|
+        ((nucleotide_a %in% pyrimidines) & (nucleotide_b %in% pyrimidines))){
+      transitions<<-append(transitions,1)
+    }
+  }
+}
+
+#define global variables
+transitions<-c()
+proba<-sapply(c(1:length(seq1)), kimura_dist)
+transitions
