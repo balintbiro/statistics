235dc3053860cc08a57b72adef80cba6fb815ec3
balintbiro <biro.balint.szie@gmail.com>
2021-12-16 12:56:33 +0100

    kimura distance init

---
+++ codes/kimura2_distance.R
@@ -0,0 +1,7 @@
+
+#given the two sequences. Let's calculate the Kimura2 distance between them!
+seq1<-'GCCGTCAGAAATTTAGCACTGATCACAGCCTCGTCTCTGA'
+seq2<-'GCCCTCAGGGAATTAGCACTAATCATAACTCCGTCTGTGT'
+
+#define the purin and pirimidin bases
+purin<-
