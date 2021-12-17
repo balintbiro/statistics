cd6af657aae5fa22d78a588696ca3a42c17df91f
balintbiro <biro.balint.szie@gmail.com>
2021-12-17 09:54:35 +0100

    function for segregating sites

--- codes/tajimad_test.R
+++ codes/tajimad_test.R
@@ -11,9 +11,20 @@ seq8<-unlist(strsplit('AAAAAAAAAAAAAAAAAAAAAAATAAAAAAAAAAAAAAAAA',split=""))
 seq9<-unlist(strsplit('AAAAAAAAAAAAAAATAAAAAAATAATAAAAAAAAAAAAAA',split=""))
 
 #load them into a list
-sequences<-list(seq0,seq1,seq2,seq3,seq4,seq5,seq6,seq7,seq8,seq9)
+sequences_list<-list(seq0,seq1,seq2,seq3,seq4,seq5,seq6,seq7,seq8,seq9)
+sequences<-c(seq0,seq1,seq2,seq3,seq4,seq5,seq6,seq7,seq8,seq9)
 
-n<-length(sequences)
+#create a dataframe from sequences vector
+sequences_df<-as.data.frame(matrix(sequences,
+                                   nrow=10,
+                                   ncol=41,
+                                   byrow=TRUE))
+
+n<-length(sequences_list)
+
+#number of segregating sites
+s<-apply(sequences_df, 2, function(column) length(unique(column)))
+s<-length(s[s!=1])/41
 
 #indices of the sequences
 indices<-c(1:(n-1))
@@ -29,4 +40,3 @@ c2<-b2-((n+2)/(a1*n))+(a2/a1**2)
 
 e1<-c1/a1
 e2<-c2/(a1**2+a2)
-
