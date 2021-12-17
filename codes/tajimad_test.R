27739559974ea527ace0af0b4f385826fc0e4df0
balintbiro <biro.balint.szie@gmail.com>
2021-12-17 08:47:37 +0100

    a1 and a2 calculation

--- codes/tajimad_test.R
+++ codes/tajimad_test.R
@@ -1,3 +1,4 @@
+#define sequences
 seq0<-unlist(strsplit('ATAATAAAAAAATAATAAAAAAATAAAAAAAATAAAAAAAA',split=""))
 seq1<-unlist(strsplit('AAAAAAAATAAATAATAAAAAAATAAAAAAAAAAAAAAAAA',split=""))
 seq2<-unlist(strsplit('AAAATAAAAATATAATAAAAAAATATAAAAAAAAAAAAAAA',split=""))
@@ -8,3 +9,12 @@ seq6<-unlist(strsplit('AAAAAATAAAAATAATAAAAAAATAAAAAAAAAAAAAAAAA',split=""))
 seq7<-unlist(strsplit('AAAAAAAAAAAAAAATAAAAAAATAAAAAAAAAAAAAAATA',split=""))
 seq8<-unlist(strsplit('AAAAAAAAAAAAAAAAAAAAAAATAAAAAAAAAAAAAAAAA',split=""))
 seq9<-unlist(strsplit('AAAAAAAAAAAAAAATAAAAAAATAATAAAAAAAAAAAAAA',split=""))
+
+#load them into a list
+sequences<-list(seq0,seq1,seq2,seq3,seq4,seq5,seq6,seq7,seq8,seq9)
+
+#indices of the sequences
+indices<-c(1:(length(sequences)-1))
+
+a1<-sum(sapply(indices, function(index) 1/index))
+a2<-sum(sapply(indices, function(index) 1/index**2))
